615579387399740372e0033f5e6e7dd9d1e0ef49
julaang5 <juliaangielczyk55@gmail.com>
2026-05-25 22:47:59 +0200

    update formsa by zapisywaly sie joki

--- src/form.js
+++ src/form.js
@@ -113,35 +113,38 @@ document.addEventListener('DOMContentLoaded', () => {
     main.appendChild(formContainer);
 
     document.body.appendChild(main);
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const jokeType = document.querySelector("#joke-type").value;
+        const jokeContent = document.querySelector("#joke-content").value;
+        const jokePunchline = document.querySelector("#joke-punchline").value;
+
+        if (
+            jokeType.trim() === "" ||
+            jokeContent.trim() === "" ||
+            jokePunchline.trim() === ""
+        ) {
+            alert("Wypełnij wszystkie pola!");
+            return;
+        }
+        const newJoke = {
+            id: Date.now(),
+            type: jokeType,
+            setup: jokeContent,
+            punchline: jokePunchline
+        };
+
+        let userJokes = JSON.parse(localStorage.getItem("userJokes")) || [];
+
+        userJokes.push(newJoke);
+
+        localStorage.setItem("userJokes", JSON.stringify(userJokes));
+
+        window.location.href = "index.html";
+    });
+
+
 });
 
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    const jokeType = document.querySelector("#joke-type").value;
-    const jokeContent = document.querySelector("#joke-content").value;
-    const jokePunchline = document.querySelector("#joke-punchline").value;
-
-    if (
-    jokeType.trim() === "" ||
-    jokeContent.trim() === "" ||
-    jokePunchline.trim() === ""
-    ) {
-    alert("Wypełnij wszystkie pola!");
-    return;
-    }
-    const newJoke = {
-        id: Date.now(),
-        type: jokeType,
-        setup: jokeContent,
-        punchline: jokePunchline
-    };
-
-    let userJokes = JSON.parse(localStorage.getItem("userJokes")) || [];
-
-    userJokes.push(newJoke);
-
-    localStorage.setItem("userJokes", JSON.stringify(userJokes));
-
-    window.location.href = "index.html";
-});
